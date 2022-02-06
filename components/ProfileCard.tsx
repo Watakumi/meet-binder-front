fff8f8831c9944665a03f16222170758f96d1dff
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-07 02:05:12 +0900

    Add TwitterItemComponent

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { GithubItem } from './GithubItem';
+import { TwitterItem } from './TwitterItem';
 export const ProfileCard: React.FC = () => {
   return (
     <div className="mt-20 flex h-96 w-full justify-center">
       <div className="h-full w-3/4 rounded-xl border-t-2 border-solid border-sky-700 shadow-lg">
         <GithubItem />
+        <TwitterItem />
       </div>
     </div>
   );
