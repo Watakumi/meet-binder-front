feac444e29632f26ac1ab8d104d1fd9f40506f8d
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-11 18:44:10 +0900

    冗長なdivを削除

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -3,11 +3,9 @@ import { GithubItem } from './GithubItem';
 import { TwitterItem } from './TwitterItem';
 export const ProfileCard: React.FC = () => {
   return (
-    <div className="mt-20 flex h-96 w-full justify-center">
-      <div className="h-full w-3/4 rounded-xl border-t-2 border-solid border-sky-700 shadow-lg">
-        <GithubItem />
-        <TwitterItem />
-      </div>
+    <div className="mt-20 h-96 w-3/4 rounded-xl border-t-2 border-solid border-sky-700 shadow-lg">
+      <GithubItem />
+      <TwitterItem />
     </div>
   );
 };
