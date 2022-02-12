f8195e03795ae6549083cd7237140b3ad0f00fd6
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-13 04:33:44 +0900

    Add TwitterLink Component

---
+++ components/TwitterLink.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineTwitter } from './Icon';
+
+const TwitterLink: React.FC = () => {
+  return (
+    <Link href="https://github.com/Watakumi" passHref>
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="github"
+        className="rounded-lg border-2 border-zinc-900 bg-zinc-800 p-2 text-2xl text-white hover:border-white"
+      >
+        <AiOutlineTwitter />
+      </a>
+    </Link>
+  );
+};
+
+export default TwitterLink;
