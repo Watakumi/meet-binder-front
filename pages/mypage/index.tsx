c82e0a08d43da0a11cdfe7ae80ac9eae408e58ac
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-13 04:33:55 +0900

    show mypage

---
+++ pages/mypage/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Mypage: React.FunctionComponent = () => {
+  return (
+    <div className="container mx-auto">
+      <div>hogehoge</div>
+    </div>
+  );
+};
+
+export default Mypage;
