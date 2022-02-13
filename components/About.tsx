ea5be4553f4a62ff39ac6028636f5818240a821a
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-13 17:34:35 +0900

    Add About Component

---
+++ components/About.tsx
@@ -0,0 +1,13 @@
+export const About = () => {
+  return (
+    <div className="font-sen">
+      <div>
+        <p className="text-3xl font-bold">Watakumi</p>
+        <p className="mt-1 text-lg font-medium text-gray-800">
+          SigntVisit Inc.
+        </p>
+        <p className="mt-4 text-gray-600">This is a test description.</p>
+      </div>
+    </div>
+  );
+};
--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { About } from '../../components/About';
 const Mypage: React.FunctionComponent = () => {
   return (
     <div className="container mx-auto">
-      <div>hogehoge</div>
+      <About />
     </div>
   );
 };
