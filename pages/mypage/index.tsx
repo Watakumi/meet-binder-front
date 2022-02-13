04509a0fb2d3262dc9bdcd7c39fe8bbd897079aa
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-13 18:05:41 +0900

    Mypageのcontainerを指定

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -3,7 +3,7 @@ import { About } from '../../components/About';
 import Skills from '../../components/Skills';
 const Mypage: React.FunctionComponent = () => {
   return (
-    <div className="container mx-auto">
+    <div className="px-2 sm:px-8 md:px-24 lg:px-48 xl:px-72">
       <About />
       <Skills />
     </div>
