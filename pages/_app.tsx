7e6b69e4bcedddda9cc5863565d312d0e4a67941
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-13 18:02:53 +0900

    Headの記述を_appに移す

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MeetBinder</title>
+        <meta name="description" content="出会いを集める。" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,6 @@ import { HeroContent } from '../layouts/HeroContent';
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>MeetBinder</title>
-        <meta name="description" content="出会いを集める。" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header />
       <HeroContent />
       <Footer />
