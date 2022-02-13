c205c6fbe4983e9ba550fe04e45aabd7eca60aba
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-13 18:00:00 +0900

    Add Skils Component

---
+++ components/Skills.tsx
@@ -0,0 +1,32 @@
+const Skills = () => {
+  return (
+    <div>
+      <p className="text-3xl">Skills</p>
+      <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
+        <a
+          className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-slate-600 p-1 text-white duration-100 hover:scale-105"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+            <p className="text-xl font-semibold">NextJS</p>
+            <p>I like NextJS as FrontendFramework.</p>
+          </div>
+        </a>
+
+        <a
+          className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-slate-600 p-1 text-white duration-100 hover:scale-105"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+            <p className="text-xl font-semibold">TypeScript</p>
+            <p>I am currently learning languages.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { About } from '../../components/About';
+import Skills from '../../components/Skills';
 const Mypage: React.FunctionComponent = () => {
   return (
     <div className="container mx-auto">
       <About />
+      <Skills />
     </div>
   );
 };
