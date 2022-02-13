0ff1a19a3851536b86f82dcd21279a524ca5de81
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-14 00:42:50 +0900

    ボタンをクリックすると詳細が表示されるようにする

--- components/Projects/ProjectItem.tsx
+++ components/Projects/ProjectItem.tsx
@@ -1,14 +1,20 @@
 type Props = {
   name: string;
   description: string;
+  onClick: any;
 };
 
-export const ProjectItem: React.FC<Props> = ({ name, description }) => {
+export const ProjectItem: React.FC<Props> = ({
+  name,
+  description,
+  onClick,
+}) => {
   return (
     <a
       className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-slate-600 p-1 text-white duration-100 hover:scale-105"
       target="_blank"
       rel="noopener noreferrer"
+      onClick={onClick}
     >
       <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
         <p className="text-xl font-semibold">{name}</p>
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+import { ProjectDetail } from './ProjectDetail';
 import { ProjectItem } from './ProjectItem';
 
 const Projects = () => {
+  const [opendId, setOpendId] = useState(-1);
+
   const projectItems = [
     {
       id: 1,
@@ -26,7 +30,33 @@ const Projects = () => {
         description: 'This is Detail of ID: 3',
       },
     },
+    {
+      id: 5,
+      name: 'NestJS',
+      description: 'NestJS is the Best Framework.',
+      detail: {
+        description: 'This is Detail of ID: 5',
+      },
+    },
+    {
+      id: 6,
+      name: 'TypeScript',
+      description: 'TypeScript is the Best Language.',
+      detail: {
+        description: 'This is Detail of ID: 6',
+      },
+    },
+    {
+      id: 4,
+      name: 'Rails',
+      description: 'Rails is the Best FullStackFramework.',
+      detail: {
+        description: 'This is Detail of ID: 4',
+      },
+    },
   ];
+
+  const project = projectItems.find((projectItem) => projectItem.id == opendId);
 
   return (
     <div className="my-8 px-3 font-sen">
@@ -37,8 +67,12 @@ const Projects = () => {
             key={projectItem.id}
             name={projectItem.name}
             description={projectItem.description}
+            onClick={() => setOpendId(projectItem.id)}
           />
         ))}
+      </div>
+      <div>
+        {project && <ProjectDetail description={project.detail.description} />}
       </div>
     </div>
   );
