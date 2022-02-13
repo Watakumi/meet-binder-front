85a9926c0568250a8f5ce16a12b020b4cb9757b1
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-14 01:23:38 +0900

    アバター画像を入れる

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,12 +1,22 @@
+import Image from 'next/image';
 export const About = () => {
   return (
-    <div className="my-8 px-3 font-sen">
+    <div className="my-8 flex flex-row items-center justify-between px-3 font-sen">
       <div>
         <p className="text-3xl font-bold">Watakumi</p>
         <p className="mt-1 text-lg font-medium text-gray-800">
           SigntVisit Inc.
         </p>
         <p className="mt-4 text-gray-600">This is a test description.</p>
+      </div>
+      <div>
+        <Image
+          src="https://picsum.photos/200"
+          width="112"
+          height="112"
+          className="rounded-full"
+          alt="avatar"
+        />
       </div>
     </div>
   );
