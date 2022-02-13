b53be6968e567895f6b2cab15bdc55019d39ac92
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-13 20:06:49 +0900

    ProjectsComponentの構成を分ける

---
+++ components/Projects/ProjectItem.tsx
@@ -0,0 +1,14 @@
+export const ProjectItem = () => {
+  return (
+    <a
+      className="h-[7rem] w-[14rem] cursor-pointer rounded-lg bg-slate-600 p-1 text-white duration-100 hover:scale-105"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
+        <p className="text-xl font-semibold">NextJS</p>
+        <p>I like NextJS as FrontendFramework.</p>
+      </div>
+    </a>
+  );
+};
---
+++ components/Projects/Projects.tsx
@@ -0,0 +1,15 @@
+import { ProjectItem } from './ProjectItem';
+
+const Projects = () => {
+  return (
+    <div className="my-8 px-3 font-sen">
+      <p className="text-3xl font-bold">Projects</p>
+      <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
+        <ProjectItem />
+        <ProjectItem />
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
