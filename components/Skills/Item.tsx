24b00f3afc00627f7cbee4a429509ae64ad8f5e0
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-14 21:22:45 +0900

    Itemのプロパティを増やす

--- components/Skills/Item.tsx
+++ components/Skills/Item.tsx
@@ -1,14 +1,15 @@
 type Props = {
   name: string;
+  description: string;
 };
 
-export const Item: React.FC<Props> = ({ name }) => {
+export const Item: React.FC<Props> = ({ name, description }) => {
   return (
     <p className="flex flex-row items-center py-1 text-gray-600">
       <span className="border-b-[0.1px] border-gray-500 text-black">
         {name}
       </span>
-      &nbsp;実務経験あり。3年ほど開発していた。
+      &nbsp;{description}
     </p>
   );
 };
