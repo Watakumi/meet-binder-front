404cc4c28c24e58dc23353b1cfc22e58f28e6247
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-16 02:02:28 +0900

    Add ProjectFormComponent

---
+++ components/Settings/Projects/ProjectForm.tsx
@@ -0,0 +1,64 @@
+const ProjectForm = () => {
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      <div className="flex flex-row items-center justify-start gap-8">
+        <div className="flex flex-row items-center gap-2">
+          <label htmlFor="project-1">Name:</label>
+          <input
+            type="text"
+            id="name-1"
+            className="
+              rounded-md
+              border-transparent
+              bg-gray-100
+              focus:border-gray-500 focus:bg-white focus:ring-0
+            "
+          />
+        </div>
+        <div className="flex w-full flex-row items-center justify-start gap-2">
+          <label htmlFor="url-1">URL:</label>
+          <input
+            type="text"
+            id="url-1"
+            className="
+              w-full
+              rounded-md
+              border-transparent
+              bg-gray-100
+              focus:border-gray-500 focus:bg-white focus:ring-0
+            "
+          />
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-start gap-4">
+        <label htmlFor="summary-1">Summary:</label>
+        <input
+          type="text"
+          id="summary-1"
+          className="
+          w-full
+          rounded-md
+          border-transparent
+          bg-gray-100
+          focus:border-gray-500 focus:bg-white focus:ring-0
+        "
+        />
+      </div>
+      <div className="flex flex-col items-start justify-center gap-4">
+        <label htmlFor="detail-1">Detail:</label>
+        <textarea
+          id="detail-1"
+          className="
+          h-24
+          w-full
+          rounded-md
+          border-transparent
+          bg-gray-100
+          focus:border-gray-500 focus:bg-white focus:ring-0
+        "
+        />
+      </div>
+    </div>
+  );
+};
+export default ProjectForm;
--- components/Settings/index.tsx
+++ components/Settings/index.tsx
@@ -2,3 +2,4 @@ export { default as EmailForm } from './EmailForm';
 export { default as TextForm } from './TextForm';
 export { default as Textarea } from './Textarea';
 export { default as SkillForm } from './Skills/SkillForm';
+export { default as ProjectForm } from './Projects/ProjectForm';
