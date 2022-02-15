aae0bacce1b96fa2cabacac4246f41c302214efa
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-15 13:16:38 +0900

    Topページのスタイルを変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 import { Footer } from '../layouts/Footer';
-import { Header } from '../layouts/Header';
+import { Header } from '../components/Header/Header';
 import { HeroContent } from '../layouts/HeroContent';
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="px-2 sm:px-8 md:px-24 lg:px-48 xl:px-72">
       <Header />
       <HeroContent />
       <Footer />
