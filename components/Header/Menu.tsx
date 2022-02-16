22261c3c7c70ce71d74a229b6b13fd8ebb45ddff
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-16 22:33:36 +0900

    Headerのボタンにリンクを当てる

--- components/Header/Menu.tsx
+++ components/Header/Menu.tsx
@@ -1,12 +1,14 @@
+import Link from 'next/link';
+
 export const Menu = () => {
   return (
     <div className="flex flex-row items-center justify-center text-center">
-      <a
-        href="#"
-        className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
-      >
-        Home
-      </a>
+      <Link href="/mypage" passHref>
+        <a className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white">
+          Home
+        </a>
+      </Link>
+
       <a
         href="#"
         className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
--- components/Header/Title.tsx
+++ components/Header/Title.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 export const Title = () => {
   return (
-    <a href="#" className="inline-block py-4">
-      <span className="p-1 text-xl font-black text-gray-900">MeetBinder</span>
-    </a>
+    <Link href="/mypage" passHref>
+      <a className="inline-block py-4">
+        <span className="p-1 text-xl font-black text-gray-900">MeetBinder</span>
+      </a>
+    </Link>
   );
 };
