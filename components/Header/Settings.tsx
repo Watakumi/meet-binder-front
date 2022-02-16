beae4f216857e81df221a0ffe3bedd0a0d2b0d0b
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-16 22:36:55 +0900

    SettingsボタンにURLを当てる

--- components/Header/Settings.tsx
+++ components/Header/Settings.tsx
@@ -1,12 +1,13 @@
+import Link from 'next/link';
+
 export const Settings = () => {
   return (
     <div>
-      <a
-        href="#"
-        className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white"
-      >
-        Settings
-      </a>
+      <Link href="/settings" passHref>
+        <a className="cursor-pointer rounded-md px-4 py-[0.10rem] text-xl text-zinc-800 duration-100 hover:bg-zinc-800 hover:text-white">
+          Settings
+        </a>
+      </Link>
     </div>
   );
 };
