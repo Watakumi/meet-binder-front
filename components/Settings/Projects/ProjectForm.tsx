53cb04baf9c718a8f696024a64555ff5668a3e20
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-18 02:14:37 +0900

    markdownの要素を表示できるようにする

--- components/Settings/Projects/ProjectForm.tsx
+++ components/Settings/Projects/ProjectForm.tsx
@@ -1,4 +1,19 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 const ProjectForm = () => {
+  const markdown = `A paragraph with *emphasis* and **strong importance**.
+
+  > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+  
+  * Lists
+  * [ ] todo
+  * [x] done
+  
+  A table:
+  
+  | a | b |
+  | - | - |`;
+
   return (
     <div className="grid grid-cols-1 gap-4">
       <div className="flex flex-row items-center justify-start gap-8">
@@ -57,6 +72,33 @@ const ProjectForm = () => {
           focus:border-gray-500 focus:bg-white focus:ring-0
         "
         />
+      </div>
+      <div className="border-md rounded-md border-[0.1px]">
+        <ul className="mt-2 flex flex-row gap-2 px-2" role="tablist">
+          <li className="">
+            <a href="" className="px-4 py-2 hover:bg-gray-100">
+              Write
+            </a>
+          </li>
+          <li className="">
+            <a href="" className="px-4 py-2 hover:bg-gray-100">
+              Preview
+            </a>
+          </li>
+        </ul>
+        <textarea
+          className="
+          mt-2
+          h-48
+          w-full
+          rounded-md
+          border-transparent
+          bg-gray-100
+          focus:border-gray-500 focus:bg-white focus:ring-0"
+        />
+        <div id="preview">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+        </div>
       </div>
     </div>
   );
