6fe9be9b42fcd927cf64736fa1813f5d664959aa
石渡拓海 <t-ishiwata@sight-visit.com>
2022-02-19 15:30:18 +0900

    writeとpreviewの切替を視覚的にできるようにする

--- components/Settings/Projects/MarkdownForm.tsx
+++ components/Settings/Projects/MarkdownForm.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { CSSProperties, useEffect, useRef, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
@@ -7,8 +7,33 @@ type markdownType = string | undefined;
 const MarkdownForm = () => {
   const markdownRef = useRef<HTMLTextAreaElement>(null);
   const [markdown, setMarkdown] = useState<markdownType>('');
-  const handleChange = () => {
-    setMarkdown(markdownRef.current?.value);
+  const [isPreviewed, setIsPreviewed] = useState(false);
+  const activatePreview = () => {
+    setIsPreviewed(true);
+  };
+
+  const deactivatePreview = () => {
+    setIsPreviewed(false);
+  };
+
+  useEffect(() => {
+    isPreviewed && setMarkdown(markdownRef.current?.value);
+  }, [isPreviewed]);
+
+  const formStyles: CSSProperties = {
+    display: isPreviewed ? 'none' : 'block',
+  };
+
+  const previewStyles: CSSProperties = {
+    display: isPreviewed ? 'block' : 'none',
+  };
+
+  const writeButtonStyles: CSSProperties = {
+    borderBottom: isPreviewed ? 'none' : 'solid 2px rgb(107 114 128)',
+  };
+
+  const previewButtonStyles: CSSProperties = {
+    borderBottom: isPreviewed ? 'solid 2px rgb(107 114 128)' : 'none',
   };
 
   return (
@@ -17,19 +42,34 @@ const MarkdownForm = () => {
       <div className="border-md rounded-md border-[0.1px]">
         <ul className="mt-2 flex flex-row gap-2 px-2" role="tablist">
           <li>
-            <a className="px-4 py-2 hover:bg-gray-100">Write</a>
+            <a
+              className="px-4 py-2 hover:bg-gray-100"
+              onClick={deactivatePreview}
+              style={writeButtonStyles}
+            >
+              Write
+            </a>
           </li>
           <li>
-            <a className="px-4 py-2 hover:bg-gray-100" onClick={handleChange}>
+            <a
+              className="px-4 py-2 hover:bg-gray-100"
+              onClick={activatePreview}
+              style={previewButtonStyles}
+            >
               Preview
             </a>
           </li>
         </ul>
         <textarea
           ref={markdownRef}
-          className="mt-2 h-48 w-full rounded-md border-transparent bg-gray-100 focus:border-gray-500 focus:bg-white focus:ring-0"
+          className="mt-4 h-48 w-full rounded-md border-transparent bg-gray-100 focus:border-gray-500 focus:bg-white focus:ring-0"
+          style={formStyles}
         />
-        <div id="preview-1">
+        <div
+          id="preview-1"
+          className="mt-4 h-48 w-full rounded-md border-[1px] border-transparent px-3 py-2"
+          style={previewStyles}
+        >
           {markdown && (
             <ReactMarkdown remarkPlugins={[remarkGfm]}>
               {markdown}
